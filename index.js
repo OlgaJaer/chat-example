9cf4b191bad07f189a5483b2b63045b9ebd5cfc9
Olga Jaeger <olgar.mamaeva@rambler.ru>
2019-10-04 16:04:12 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+users = [];
+connections = [];
+
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on *:3000');
+});
+
+app.use('/public', express.static('public'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket) {
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length);
+  
+  //Disconnect
+  socket.on('disconnect', function(data) {
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Disconnected: %s sockets connected', connections.length);
+  });
+
+  //send message
+  socket.on('send message', function(data){
+    io.sockets.emit('new message', {msg: data, user: socket.username});
+  });
+
+  //new user
+  socket.on('new user', function(data, cb){
+    cb(true);
+    socket.username = data.user;
+    users.push(socket.username);
+    updateUsernames();
+  });
+
+  function updateUsernames(){
+    io.sockets.emit('get users', users);
+  } 
+});
+
