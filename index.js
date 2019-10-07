b932fb2c8c32d136dbe596bdc1b687cfe31f1aaf
Olga Jaeger <olgar.mamaeva@rambler.ru>
2019-10-07 11:05:20 +0200

    changed authoriz

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-users = [];
 connections = [];
+users = [];
 
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:3000');
@@ -15,12 +15,22 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function(socket) {
+/*app.post('/public', function(req, res){
+  if(req.body.name === 'Olga' && req.body.password === 'olga') {
+    res.send('ok');
+  } else {
+    res.send('some mistake');
+  }
+});*/
+
+//let numUsers = 0;
+
+io.sockets.on('connection', (socket) => {
   connections.push(socket);
   console.log('Connected: %s sockets connected', connections.length);
-  
+
   //Disconnect
-  socket.on('disconnect', function(data) {
+  socket.on('disconnect', (data) => {
     users.splice(users.indexOf(socket.username), 1);
     updateUsernames();
     connections.splice(connections.indexOf(socket), 1);
@@ -28,7 +38,7 @@ io.sockets.on('connection', function(socket) {
   });
 
   //send message
-  socket.on('send message', function(data){
+  socket.on('send message', (data) => {
     io.sockets.emit('new message', {msg: data, user: socket.username});
   });
 
@@ -44,4 +54,3 @@ io.sockets.on('connection', function(socket) {
     io.sockets.emit('get users', users);
   } 
 });
-
